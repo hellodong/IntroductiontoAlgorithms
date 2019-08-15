0b565fe1d17f50c3e79a54d6fa428939f053aa63
purefarmer <puerfarmer@protonmail.com>
2019-08-15 20:18:11 +0800

    test(): 添加删除，测试节点数量

--- ch13_rbTree/main.c
+++ ch13_rbTree/main.c
@@ -34,17 +34,50 @@ int main(int argc, char *argv[])
 	nodePtr->key = 10;
 	rbTreexInsert(&tree, nodePtr);
 
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 18;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 20;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 24;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 30;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 31;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 34;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 36;
+	rbTreexInsert(&tree, nodePtr);
+
+	nodePtr = (stRbNode_t *)malloc(sizeof(*nodePtr));
+	nodePtr->key = 40;
+	rbTreexInsert(&tree, nodePtr);
+
     rbTreexInorderWalk(&tree);
 
-	key = 11;
+	key = 40;
 	nodePtr = rbTreexSearch(&tree, key);
 	if (nodePtr != tree.nil){
 		printf("Found Key:%d\r\n", nodePtr->key);
+		rbTreexDel(&tree, nodePtr);
 	}else{
 		printf("Can't found Key\r\n");
 	}
 
-
+    rbTreexInorderWalk(&tree);
 
     return 0;
 }
